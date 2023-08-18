0c3db15da8b95a01ab485887334cf031e896f8e1
Nils <nilsbaumgartner1994@gmail.com>
2023-08-19 01:43:37 +0200

    Update

--- RocketMealsDocumentation/__init__.py
+++ RocketMealsDocumentation/__init__.py
@@ -1,4 +1,1 @@
-from RocketMealsDocumentation.integration import store_results, create_device_targets
-from RocketMealsDocumentation.interface import tune_kernel, run_kernel
-
-__version__ = "0.4.5"
+__version__ = "0.0.1"
