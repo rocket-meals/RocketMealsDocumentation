28af7c22e7472a2d273c508fa1ffab03683cff1e
Nils <nilsbaumgartner1994@gmail.com>
2023-08-19 11:27:25 +0200

    Changed primary color

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -121,6 +121,9 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+html_theme_options = {
+    'primary_color': '#FF0000',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
